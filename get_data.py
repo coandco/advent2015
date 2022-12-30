b245a9bd4f623ae9eaec856c411faf270ef5f5fe
Clint Olson <coandco@gmail.com>
2022-12-30 15:42:50 -0800

    Add framework

---
+++ get_data.py
@@ -0,0 +1,31 @@
+from aocd.models import Puzzle
+from pathlib import Path
+
+# This is a standalone script meant to be run to automatically grab my data
+# for a given year/day and dump it into an automatically-named file.
+
+YEAR_NUM = 2015
+DAY_NUM = 1
+
+puzzle = Puzzle(year=YEAR_NUM, day=DAY_NUM)
+
+file_location = Path(f'inputs/advent{YEAR_NUM}_day{DAY_NUM:02d}_input.txt')
+file_location.write_text(puzzle.input_data)
+
+program_location = Path(f"advent{YEAR_NUM}_day{DAY_NUM:02d}.py")
+blank_day = """from utils import read_data
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    import time
+    start = time.monotonic()
+    main()
+    print(f"Time: {time.monotonic()-start}")
+"""
+
+if not program_location.exists():
+    program_location.write_text(blank_day)
